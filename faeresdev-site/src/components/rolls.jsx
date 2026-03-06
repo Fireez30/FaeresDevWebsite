e5ee65690a727bb95a32cbd85ad70064e83e380b
Fireez30 <benjamingallean34@proton.me>
2026-03-06 15:20:20 +0100

    Added card rolls

--- faeresdev-site/src/components/rolls.jsx
+++ faeresdev-site/src/components/rolls.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {setGender, setLevel} from "../features/pokemon/pokemonSlice.jsx";
 import Button from 'react-bootstrap/Button';
-import {InputNumber} from "antd";
+import {Checkbox, InputNumber} from "antd";
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -28,23 +28,86 @@ function eraseCookie(name) {
 
 function Rolls() {
     const [dexnav, setDexnav] = useState(0);
+    const [card_dexnav, setCardDexnav] = useState(0);
     const [pokemonShinyRoll, setPokemonShinyRoll] = useState(1);
     const [number_of_shiny_to_roll, setNumberOfShinyToRoll] = useState(1);
+    const [number_of_cards_to_roll, setNumberOfCardsToRoll] = useState(1);
     const [cardRoll1, setcardRoll1] = useState(1);
     const [cardRoll2, setcardRoll2] = useState(33);
     const [cardRoll3, setcardRoll3] = useState(66);
     const [cardShinyRoll1, setcardShinyRoll1] = useState(22);
     const [cardShinyRoll2, setcardShinyRoll2] = useState(44);
+    const [cardPlatineRoll1, setcardPlatineRoll1] = useState(100);
     const [rolled_encounter_bracket,setRolledEncounterBracket] = useState(-1);
     const [roll_capture_accuracy,setRollCaptureAccuracy] = useState(-1);
     const [roll_capture_rate,setRollCaptureRate] = useState(-1);
     const [rolled_encounter_count,setRolledEncounterCount] = useState(-1);
     const [list_rolls_shiny_poke,setListRollsShinyPoke] = useState([]);
+    const [list_rolls_normal_cards,setListRollsNormalCards] = useState([]);
+    const [is_pokemon_shiny,setIsPokemonShiny] = useState(false);
+    const [is_pokemon_platine,setIsPokemonPlatine] = useState(false);
+
+    if (getCookie("cardRoll1")){
+        let cookie_value = parseInt(getCookie("cardRoll1"));
+        if (cookie_value !== cardRoll1){
+            setcardRoll1(parseInt(getCookie("cardRoll1")));
+        }
+
+    }
+
+    if (getCookie("cardRoll2")){
+        let cookie_value = parseInt(getCookie("cardRoll2"));
+        if (cookie_value !== cardRoll2){
+            setcardRoll2(parseInt(getCookie("cardRoll2")));
+        }
+
+    }
+
+    if (getCookie("cardRoll3")){
+        let cookie_value = parseInt(getCookie("cardRoll3"));
+        if (cookie_value !== cardRoll3){
+            setcardRoll3(parseInt(getCookie("cardRoll3")));
+        }
+
+    }
+
+    if (getCookie("cardShinyRoll1")){
+        let cookie_value = parseInt(getCookie("cardShinyRoll1"));
+        if (cookie_value !== cardShinyRoll1){
+            setcardShinyRoll1(parseInt(getCookie("cardShinyRoll1")));
+        }
+
+    }
+
+    if (getCookie("cardShinyRoll2")){
+        let cookie_value = parseInt(getCookie("cardShinyRoll2"));
+        if (cookie_value !== cardShinyRoll2){
+            setcardShinyRoll2(parseInt(getCookie("cardShinyRoll2")));
+        }
+
+    }
+
+    if (getCookie("cardPlatineRoll1")){
+        let cookie_value = parseInt(getCookie("cardPlatineRoll1"));
+        if (cookie_value !== cardPlatineRoll1){
+            setcardPlatineRoll1(parseInt(getCookie("cardPlatineRoll1")));
+        }
+
+    }
+
 
     if (getCookie("dexnav")){
         let cookie_value = parseInt(getCookie("dexnav"));
         if (cookie_value !== dexnav){
             setDexnav(parseInt(getCookie("dexnav")));
+        }
+
+    }
+
+    if (getCookie("card_dexnav")){
+        let cookie_value = parseInt(getCookie("card_dexnav"));
+        if (cookie_value !== card_dexnav){
+            setCardDexnav(parseInt(getCookie("card_dexnav")));
         }
 
     }
@@ -99,7 +162,7 @@ function Rolls() {
                 Your shiny number :
                 <InputNumber
                 min={1}
-                max={99}
+                max={100}
                 defaultValue={1}
                 value={pokemonShinyRoll}
                 onChange={value => {
@@ -135,7 +198,7 @@ function Rolls() {
                 Number of dice rolls :
                 <InputNumber
                     min={1}
-                    max={99}
+                    max={100}
                     defaultValue={1}
                     value={number_of_shiny_to_roll}
                     onChange={value => {
@@ -153,9 +216,11 @@ function Rolls() {
                     if (pokemonShinyRoll < dexnav){ // if we roll under dexnav, we have to shift the reroll window
                         reroll_under_or_equal += 1;
                     }
+                    reroll_under_or_equal = Math.min(90,reroll_under_or_equal);
                     Array(number_of_shiny_to_roll).keys().forEach(key => {
                         rolls.push(getRandomArbitrary(0,100)+1);
                     })
+
                     while (rolls.length > 0){
                         rolls.map((roll,index) => {
                             if (roll > reroll_under_or_equal || roll === pokemonShinyRoll) {
@@ -180,7 +245,7 @@ function Rolls() {
                         <text>List of rolls : </text>
                         {
                             list_rolls_shiny_poke.map((roll,index) => {
-                                return <text style={{'color':(roll===pokemonShinyRoll?'#00ff14':roll===100?'cyan':'white')}}>{roll} </text>
+                                return <text style={{'color':(roll===pokemonShinyRoll?'#00ff14':(roll===100 && list_rolls_shiny_poke.find(poke => {return poke === pokemonShinyRoll}))?'cyan':'white')}}>{roll} </text>
                             })
                         }
 
@@ -189,6 +254,226 @@ function Rolls() {
             </div >
             <div >
             <h3>Card rolls</h3>
+                Your normal cards numbers :
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={cardRoll1}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("cardRoll1")){
+                                eraseCookie("cardRoll1");
+                            }
+                            writeCookie("cardRoll1", value);
+                            setcardRoll1(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber>
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={cardRoll2}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("cardRoll2")){
+                                eraseCookie("cardRoll2");
+                            }
+                            writeCookie("cardRoll2", value);
+                            setcardRoll2(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber>
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={cardRoll3}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("cardRoll3")){
+                                eraseCookie("cardRoll3");
+                            }
+                            writeCookie("cardRoll3", value);
+                            setcardRoll3(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber> <br></br>
+                Your shiny cards numbers :
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={cardShinyRoll1}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("cardShinyRoll1")){
+                                eraseCookie("cardShinyRoll1");
+                            }
+                            writeCookie("cardShinyRoll1", value);
+                            setcardShinyRoll1(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber>
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={cardShinyRoll2}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("cardShinyRoll2")){
+                                eraseCookie("cardShinyRoll2");
+                            }
+                            writeCookie("cardShinyRoll2", value);
+                            setcardShinyRoll2(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber>
+                <Checkbox
+                    style={{marginLeft:'5px'}}
+                    checked={is_pokemon_shiny}
+                    onChange={e => {
+                        setIsPokemonShiny(e.target.checked);
+                }}>
+                    Is caught pokemon shiny ?
+                </Checkbox> <br/>
+                Your platine cards number :
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={cardPlatineRoll1}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("cardPlatineRoll1")){
+                                eraseCookie("cardPlatineRoll1");
+                            }
+                            writeCookie("cardPlatineRoll1", value);
+                            setcardPlatineRoll1(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber><Checkbox
+                style={{marginLeft:'5px'}}
+                checked={is_pokemon_platine}
+                onChange={e => {
+                    setIsPokemonPlatine(e.target.checked);
+                }}>
+                Is caught pokemon platine ?
+            </Checkbox><br></br>
+                Card Dexnav :
+                <InputNumber
+                    min={1}
+                    max={90}
+                    defaultValue={1}
+                    value={card_dexnav}
+                    onChange={value => {
+                        if (value){
+                            if (getCookie("card_dexnav")){
+                                eraseCookie("card_dexnav");
+                            }
+                            writeCookie("card_dexnav", value);
+                            setCardDexnav(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber>
+                <br></br>
+                Number of dice rolls :
+                <InputNumber
+                    min={1}
+                    max={100}
+                    defaultValue={1}
+                    value={number_of_cards_to_roll}
+                    onChange={value => {
+                        if (value){
+                            setNumberOfCardsToRoll(value);
+                        }
+
+                    }}
+                    style={{marginLeft:'5px',marginTop:'5px',width:'8%',height:'30px'}}>
+                </InputNumber> <br></br>
+                <Button style={{marginLeft:'5px',marginRight:'5px',marginTop:'5px',width:'5%',height:'30px'}} onClick={() => {
+                    let rolls = [];
+                    let final_rolls = [];
+                    let reroll_under_or_equal = card_dexnav;
+                    if (cardRoll1 < card_dexnav){ // if we roll under dexnav, we have to shift the reroll window
+                        reroll_under_or_equal += 1;
+                    }
+                    if (cardRoll2 < card_dexnav){ // if we roll under dexnav, we have to shift the reroll window
+                        reroll_under_or_equal += 1;
+                    }
+                    if (cardRoll3 < card_dexnav){ // if we roll under dexnav, we have to shift the reroll window
+                        reroll_under_or_equal += 1;
+                    }
+                    if (cardShinyRoll1 < card_dexnav){ // if we roll under dexnav, we have to shift the reroll window
+                        reroll_under_or_equal += 1;
+                    }
+
+                    if (cardShinyRoll2 < card_dexnav){ // if we roll under dexnav, we have to shift the reroll window
+                        reroll_under_or_equal += 1;
+                    }
+                    if (cardPlatineRoll1 < card_dexnav){ // if we roll under dexnav, we have to shift the reroll window
+                        reroll_under_or_equal += 1;
+                    }
+                    reroll_under_or_equal = Math.min(90,reroll_under_or_equal);
+
+
+                    Array(number_of_cards_to_roll).keys().forEach(key => {
+                        rolls.push(getRandomArbitrary(0,100)+1);
+                    })
+                    while (rolls.length > 0){
+                        rolls.map((roll,index) => {
+                            if (roll > reroll_under_or_equal ||
+                                roll === cardRoll1 ||
+                                roll === cardRoll2 ||
+                                roll === cardRoll3 ||
+                                ((is_pokemon_platine || is_pokemon_shiny) && (roll === cardShinyRoll1 || roll === cardShinyRoll2)) ||
+                                (is_pokemon_platine && (roll === cardPlatineRoll1))
+                            ) {
+                                final_rolls.push(roll);
+                                rolls.splice(index,1);
+                            }
+                        })
+                        let to_reroll = rolls.length;
+                        rolls = [];
+                        Array(to_reroll).keys().forEach(key => {
+                            rolls.push(getRandomArbitrary(0,100)+1);
+                        })
+                    }
+                    setListRollsNormalCards(final_rolls);
+
+                }}>
+                    Roll
+                </Button>
+                {
+                list_rolls_normal_cards && list_rolls_normal_cards.length > 0 &&
+                <div>
+                    <text>List of rolls : </text>
+                    {
+                        list_rolls_normal_cards.map((roll,index) => {
+                            return <text style={{'color':(roll===cardRoll1||roll===cardRoll2||roll===cardRoll3?'#00ff14':((is_pokemon_platine || is_pokemon_shiny )&& (roll===cardShinyRoll1||roll===cardShinyRoll2))?'lightyellow':(is_pokemon_platine && (roll===cardPlatineRoll1))?'cyan':'white')}}>{roll} </text>
+                        })
+                    }
+                    <br></br>
+                    <br></br>
+
+                </div>
+                }
             </div>
         </div>
     );
